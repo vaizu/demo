5b18c67f6b34c85f92a6045568ffb43ec20f964a
vaizu <vaizu07204869@yahoo.co.jp>
2022-05-28 05:39:02 +0000

    [hazimete]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
   root to: "homes#top"
+
+  get 'hazimete' => 'homes#hazimete'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
